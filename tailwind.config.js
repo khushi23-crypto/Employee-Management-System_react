9a965aaf43ccd8493b94008a03fd39665c6dfecd
Kajal Saini <sizukabts91@gmail.com>
2025-09-06 19:47:48 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // All files Tailwind should scan
+  ],
+  theme: {
+    extend: {}, // For custom theme extensions
+  },
+  plugins: [], // Optional plugins
+};

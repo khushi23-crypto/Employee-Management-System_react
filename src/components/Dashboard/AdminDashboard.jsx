b95ba2393f110b1e211e37d6c70902782209b8e4
Kajal Saini <sizukabts91@gmail.com>
2025-09-07 20:44:39 +0530

    Create Task

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../Other/Header'
+import CreateTask from '../Other/CreateTask'
 
 const AdminDashboard = () => {
-  return (
-    <div className='h-screen w-full p-10'>
-        <Header/>
-    </div>
-  )
+    return (
+        <div className='h-screen w-full p-7'>
+            <Header />
+            <CreateTask/>
+           
+        </div>
+    )
 }
 
 export default AdminDashboard

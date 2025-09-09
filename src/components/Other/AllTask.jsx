7000b172bfe3aa90728d31375989645ae3f1fb12
Kajal Saini <sizukabts91@gmail.com>
2025-09-09 19:42:27 +0530

    set tasks related things

--- src/components/Other/AllTask.jsx
+++ src/components/Other/AllTask.jsx
@@ -8,24 +8,24 @@ const AllTask = () => {
   return (
     <div className='bg-[#1c1c1c] p-5 mt-5 rounded '>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2 className='w-1/5 '>Employee Name</h2>
-        <h3 className='w-1/5 '>New Task</h3>
-        <h5 className='w-1/5 '>Active Task</h5>
-        <h5 className='w-1/5 '>Completed Task</h5>
-        <h5 className='w-1/5 '>Failed Task</h5>
+        <h2 className='text-lg font-medium w-1/5 '>Employee Name</h2>
+        <h3 className='text-lg font-medium w-1/5 '>New Task</h3>
+        <h5 className='text-lg font-medium w-1/5 '>Active Task</h5>
+        <h5 className='text-lg font-medium w-1/5 '>Completed Task</h5>
+        <h5 className='text-lg font-medium w-1/5 '>Failed Task</h5>
 
 
       </div>
       <div className=' overflow-auto'>
-        {authData.employees.map(function (elem) {
-        return <div className= 'border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-          <h2 className='w-1/5 '>{elem.firstname}</h2>
-          <h3 className='w-1/5 text-blue-600'>Task</h3>
-          <h5 className='w-1/5 text-yellow-300'>Status</h5>
-          <h5 className='w-1/5 text-white'>Status</h5>
-          <h5 className='w-1/5 text-red-600'>Failed</h5>
-        </div>
-      })}
+        {authData.employees.map(function (elem,idx) {
+          return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='text-lg font-medium w-1/5 '>{elem.firstname}</h2>
+            <h3 className='text-lg font-medium  w-1/5 text-blue-600'>{elem.taskNumbers.new_task}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-300'>{elem.taskNumbers.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskNumbers.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskNumbers.failed}</h5>
+          </div>
+        })}
       </div>
     </div>
   )
